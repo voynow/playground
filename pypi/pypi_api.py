db328002a5a9d8217059a828e729f0555140ccf0
voynow <voynow99@gmail.com>
2023-12-23 10:55:47 -0500

    Rewriting extract pipeline

--- pypi/pypi_api.py
+++ pypi/pypi_api.py
@@ -4,17 +4,6 @@ import tarfile
 import requests
 from bs4 import BeautifulSoup
 
-LICENSE_WHITELIST = [
-    "Apache-2.0",
-    "Apache Software License",
-    "MIT",
-    "BSD",
-    "ISC",
-    "LGPL",
-    "MPL-2.0",
-    "Unlicense",
-]
-
 
 def get_packages():
     """
@@ -42,13 +31,42 @@ def get_package_metadata(package_name: str) -> dict:
     }
 
 
-def download_source_code(package_metadata: dict, target_dir: str):
-    # Extract package info
+def extract_from_tarball(name: str, target_dir: str, tar_path: str):
+    """Write tar contents to target directory"""
+    if os.path.exists(tar_path) and os.path.getsize(tar_path) > 0:
+        try:
+            with tarfile.open(tar_path) as tar:
+                tar.extractall(path=target_dir)
+        except tarfile.ReadError:
+            print(f"({name}) Failed to read {tar_path}.")
+    else:
+        print(f"({name}) {tar_path} does not exist or is empty.")
+
+
+def download_tarball(url: str, name: str, target_dir: str):
+    """Download from a given URL and extract to the target directory"""
+
+    # create the target directory if it doesn't exist
+    os.makedirs(target_dir, exist_ok=True)
+
+    # download tar and save to target directory
+    response = requests.get(url)
+    if response.status_code == 200:
+        tar_path = os.path.join(target_dir, f"{name}.tar.gz")
+        with open(tar_path, "wb") as file:
+            file.write(response.content)
+
+        # extract from tar & clean up
+        extract_from_tarball(name, target_dir, tar_path)
+        os.remove(tar_path)
+    else:
+        print(f"({name}) Failed to download {url}")
+
+
+def download_source_code(package_metadata: dict, target_dir: str) -> None:
+    """Download the source code for a given package metadata"""
     name = package_metadata["info"]["name"]
     version = package_metadata["info"]["version"]
-
-    # Ensure the directory exists
-    os.makedirs(target_dir, exist_ok=True)
 
     # Find the source distribution URL (.tar.gz)
     for release in package_metadata["releases"][version]:
@@ -59,21 +77,4 @@ def download_source_code(package_metadata: dict, target_dir: str):
         print(f"({name}) No source distribution found for {name}-{version}")
         return
 
-    # Download the source distribution
-    response = requests.get(url)
-    if response.status_code == 200:
-        tar_path = os.path.join(target_dir, f"{name}.tar.gz")
-        with open(tar_path, "wb") as file:
-            file.write(response.content)
-
-        if os.path.exists(tar_path) and os.path.getsize(tar_path) > 0:
-            try:
-                with tarfile.open(tar_path) as tar:
-                    tar.extractall(path=target_dir)
-            except tarfile.ReadError:
-                print(f"({name}) Failed to read {tar_path}. It may not be a tar archive.")
-        else:
-            print(f"({name}) {tar_path} does not exist or is empty.")
-        os.remove(tar_path)
-    else:
-        print(f"({name}) Failed to download {url}")
+    download_tarball(url, name, target_dir)
